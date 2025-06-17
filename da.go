2ba8e995ef3238c2be996958ca818e9c65d39395
kveze <127878304+kveze@users.noreply.github.com>
2025-06-17 09:06:27 +0300

    del func

--- da.go
+++ da.go
@@ -1,16 +1,13 @@
 package main
+
 import (
 	"fmt"
 	"math/rand"
 )
+
 func main() {
 	da(rand.Intn(3))
 }
 func da(x int) {
-	for i := 1; i <= 5; i++ {
-		if x <= rand.Intn(3) {
-			fmt.Println("CHANCE 33.3%")
-		}
-	}
-
+	fmt.Println(x)
 }
