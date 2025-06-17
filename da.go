01c16e878af587e8f627929d7b1ae60c1cfc1d92
kveze <127878304+kveze@users.noreply.github.com>
2025-06-17 09:17:43 +0300

    Update da.go

--- da.go
+++ da.go
@@ -5,12 +5,21 @@ import (
 )
 func main() {
 	da(rand.Intn(3))
+	net(rand.Intn(11))
 }
 func da(x int) {
 	for i := 1; i <= 5; i++ {
-		if x <= rand.Intn(3) {
+		if x == rand.Intn(3) {
 			fmt.Println("CHANCE 33.3%")
 		}
+		fmt.Println(i)
 	}
 
+}
+func net(k int)  {
+	if k % 2 == 0{
+		fmt.Println("delet", k)
+	} else {
+		fmt.Println("nedelet", k)
+	}
 }
